1ce4ca778223b53bd274a7a5c8a951ef20bf63ea
abdulaziz <abdulaziz2013.uz@gmail.com>
2026-09-16 15:48:21 +0500

    Fix book image serving on Render

--- config/urls.py
+++ config/urls.py
@@ -15,17 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
 ]
 
-# Rasm va fayllar (media) brauzerda ochilishi uchun
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# WhiteNoise faqat static fayllar uchun ishlaydi; kitob rasmlari media fayllardir.
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    )
+]
 
 
 
